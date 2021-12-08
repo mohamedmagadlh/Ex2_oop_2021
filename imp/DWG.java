09feee9b67a53f28ef0770dd0949bfa454e015fb
mohamedmagadlh <mohamed1998magadlh@gmail.com>
2021-12-08 15:00:39 +0200

    DGW and NodeData done!

---
+++ imp/DWG.java
@@ -0,0 +1,126 @@
+package api.imp;
+
+import api.api.DirectedWeightedGraph;
+import api.api.EdgeData;
+import api.api.NodeData;
+import java.util.Set;
+import java.util.*;
+
+public class DWG implements DirectedWeightedGraph {
+      NodeData nodeData;
+     HashMap<Integer, NodeData> NodesHash;
+     HashMap<Integer,HashMap<Integer,EdgeData>>EdgesHash;
+     int MC;
+    public DWG()
+    {
+        this.nodeData=null;
+       this.NodesHash=new HashMap<Integer, NodeData> ();
+       this.EdgesHash=new HashMap<Integer,HashMap<Integer,EdgeData>>();
+        this.MC=0;
+    }
+    @Override
+    public NodeData getNode(int key) {
+        return NodesHash.get(key);
+    }
+    @Override
+    public EdgeData getEdge(int src, int dest) {
+        if(NodesHash.containsKey(src) && NodesHash.containsKey(dest) &&EdgesHash.get(src).containsKey(dest))
+            return EdgesHash.get(src).get(dest);
+        return null;
+    }
+    @Override
+    public void addNode(NodeData n) {
+        if(NodesHash.containsKey(n.getKey()) || n.getWeight()<=0)
+        {
+            System.err.println("ADD IS FAIL");
+            return;
+        }
+        NodesHash.put(n.getKey(),n);
+        EdgesHash.put(n.getKey(),new HashMap<Integer,EdgeData>());
+        MC++;
+    }
+
+    @Override
+    public void connect(int src, int dest, double w) {
+    EdgeDataClass e=new EdgeDataClass(src,dest,w);
+    if(NodesHash.containsKey(src)&& NodesHash.containsKey(dest) && w>0)
+        EdgesHash.get(src).put(dest,e);
+    else {
+        System.err.println("CONNECT FAIL\nn");
+        return;
+    }
+    MC++;
+    }
+    @Override
+    public Iterator<NodeData> nodeIter() {
+        return this.NodesHash.values().iterator();
+    }
+
+    @Override
+    public Iterator<EdgeData> edgeIter() {
+        return (Iterator)this.EdgesHash.values().iterator();
+    }
+
+    @Override
+    public Iterator<EdgeData> edgeIter(int node_id) {
+        return null;
+    }
+
+    @Override
+    public NodeData removeNode(int key) {
+        if(NodesHash.containsKey(key)) {
+            NodesHash.remove(key);
+            Set<Integer> Keys=EdgesHash.keySet();
+            for(Integer e:Keys)
+            {
+                if(EdgesHash.get((int)e).containsKey(key))
+                    EdgesHash.get((int)e).remove(key);
+            }
+        }
+        else {
+            System.err.println("REMOVE NODE FAIL\n");
+            return null;
+        }
+        MC++;
+        return this.NodesHash.remove(key);
+    }
+
+    @Override
+    public EdgeData removeEdge(int src, int dest) {
+        if(getEdge(src,dest)==null)return null;
+        EdgeDataClass e=new EdgeDataClass(src,dest);
+        EdgesHash.get(src).remove(dest);
+        MC++;
+        return e;
+    }
+
+    @Override
+    public int nodeSize() {
+        return NodesHash.size();
+    }
+
+    @Override
+    public int edgeSize() {
+        return EdgesHash.size();
+    }
+
+    @Override
+    public int getMC() {
+        return MC;
+
+    }
+    public void setMC(int m)
+    {
+        this.MC=m;
+    }
+
+    @Override
+    public String toString() {
+        return "DirectedWeightedGraphClass{" +
+                "nodeData=" + nodeData +
+                ", NodesHash=" + NodesHash +
+                ", EdgesHash=" + EdgesHash +
+                ", MC=" + MC +
+                '}';
+    }
+}
---
+++ imp/nodeDataimp.java
@@ -0,0 +1,83 @@
+package api.imp;
+
+import api.api.GeoLocation;
+import api.api.NodeData;
+
+import java.util.ArrayList;
+import java.util.Collection;
+class nodeDataimp implements NodeData {
+    private int key;
+    private GeoLocation geoLocation;
+    private double weight;
+    private int tag;
+    private String info;
+    private static int white = 0;
+    private static int black = 1;
+    private static int gray = 2;
+    public nodeDataimp(int key, GeoLocation geoLocation, double weight, int tag, String info)
+    {
+        this.key=key;
+        this.geoLocation=geoLocation;
+        this.weight=weight;
+        this.tag=tag;
+        this.info=info;
+    }
+
+
+    @Override
+    public int getKey() {
+        return this.key;
+    }
+
+    @Override
+    public GeoLocation getLocation() {
+        if(this.geoLocation==null)return null;
+        return this.geoLocation;
+    }
+
+    @Override
+    public void setLocation(GeoLocation p) {
+        this.geoLocation=p;
+    }
+
+    @Override
+    public double getWeight() {
+        return this.weight;
+    }
+
+    @Override
+    public void setWeight(double w) {
+        this.weight=w;
+    }
+
+    @Override
+    public String getInfo() {
+        return this.info;
+    }
+
+    @Override
+    public void setInfo(String s) {
+        this.info=s;
+    }
+
+    @Override
+    public int getTag() {
+        return this.tag;
+    }
+
+    @Override
+    public void setTag(int t) {
+        this.tag=t;
+    }
+
+    @Override
+    public String toString() {
+        return "NodeDataCLass{" +
+                "key=" + key +
+                ", geoLocation=" + geoLocation +
+                ", weight=" + weight +
+                ", tag=" + tag +
+                ", info='" + info + '\'' +
+                '}';
+    }
+}
